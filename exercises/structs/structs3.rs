1103d8528a965bc660a97ae088e45c95c75f45bb
thejas <thejasb99@gmail.com>
2020-07-17 13:24:06 +0530

    exercises-structs done

--- exercises/structs/structs3.rs
+++ exercises/structs/structs3.rs
@@ -15,18 +15,18 @@ struct Package {
 impl Package {
     fn new(sender_country: String, recipient_country: String, weight_in_grams: i32) -> Package {
         if weight_in_grams <= 0 {
-            // Something goes here...
+            panic!("Negative weight")
         } else {
             return Package {sender_country, recipient_country, weight_in_grams};
         }
     }
 
-    fn is_international(&self) -> ??? {
-        // Something goes here...
+    fn is_international(&self) -> bool {
+        return if self.sender_country != self.recipient_country { true } else { false }
     }
 
-    fn get_fees(&self, cents_per_kg: i32) -> ??? {
-        // Something goes here... (beware of grams to kg conversion)
+    fn get_fees(&self, cents_per_kg: i32) -> i32 {
+        return self.weight_in_grams * (cents_per_kg / 1000)
     }
 }
 
@@ -58,7 +58,7 @@ mod tests {
         let sender_country = String::from("Spain");
         let recipient_country = String::from("Spain");
 
-        let cents_per_kg = ???;
+        let cents_per_kg = 3000;
 
         let package = Package::new(sender_country, recipient_country, 1500);
 
